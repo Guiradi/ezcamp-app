bcc2ef1cacc74a2127771a6a16bdfbc543d41773
Guiradi <g.ferreira@unesp.br>
2020-01-18 16:37:42 -0300

    Initialized main screen card

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import {View, Image} from 'react-native';
+import React, {useCallback} from 'react';
+import {View, Image, TouchableOpacity} from 'react-native';
 
 import logo from '../assets/images/logo.png';
 
-export default () => (
-  <View
-    style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      backgroundColor: '#f5f5f5',
-    }}>
-    <Image source={logo} />
-  </View>
-);
+export default ({navigation: {navigate}}) => {
+  const onLogout = useCallback(() => {
+    navigate('login');
+  }, [navigate]);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: '#f5f5f5',
+      }}>
+      <TouchableOpacity onPress={onLogout}>
+        <Image source={logo} />
+      </TouchableOpacity>
+
+      <View
+        style={{
+          borderWidth: 1,
+          borderColor: '#ddd',
+          borderRadius: 8,
+          margin: 30,
+          elevation: 1,
+        }}>
+        <Image
+          style={{
+            height: 300,
+            width: 300,
+          }}
+          source={null}
+        />
+      </View>
+    </View>
+  );
+};

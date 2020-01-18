4bfb9e28ad26e33ecf4bd7867068ffac88f58c27
Guiradi <g.ferreira@unesp.br>
2020-01-18 14:51:55 -0300

    Added username value watcher

--- App.js
+++ App.js
@@ -1,30 +1,57 @@
-import React from 'react';
-import {View, Image, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {View, Image, TextInput, TouchableOpacity, Text} from 'react-native';
 
 import logo from './src/assets/images/logo.png';
 
-export default () => (
-  <View
-    style={{
-      flex: 1,
-      backgroundColor: '#f5f5f5',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-    <Image source={logo} />
-    <TextInput
-      autoCapitalize="none"
-      placeholder="Digite seu usuário no GitHub"
+export default () => {
+  const [username, setUsername] = useState('');
+
+  console.log(username);
+
+  return (
+    <View
       style={{
-        marginTop: 20,
-        height: 45,
-        backgroundColor: '#fff',
-        borderWidth: 1,
-        borderColor: '#ddd',
-        borderRadius: 4,
-        alignSelf: 'stretch',
-        marginHorizontal: 15,
-      }}
-    />
-  </View>
-);
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        paddingHorizontal: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Image source={logo} />
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Digite seu usuário no GitHub"
+        value={username}
+        onChangeText={setUsername}
+        style={{
+          marginTop: 20,
+          height: 45,
+          backgroundColor: '#fff',
+          borderWidth: 1,
+          borderColor: '#ddd',
+          borderRadius: 4,
+          alignSelf: 'stretch',
+        }}
+      />
+      <TouchableOpacity
+        style={{
+          marginTop: 10,
+          height: 45,
+          alignSelf: 'stretch',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#DF4723',
+          borderRadius: 4,
+        }}>
+        <Text
+          style={{
+            color: '#fff',
+            fontWeight: 'bold',
+            fontSize: 16,
+          }}>
+          ENTRAR
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
